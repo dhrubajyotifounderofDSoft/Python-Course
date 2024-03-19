72025c2716746f6ecdd8185f6c0f9102b6766918
Dhrubajyoti Biswas <dsofthelp.org@outlook.com>
2024-03-19 10:37:45 +0530

    Create Chapter_7.py

---
+++ Chapter_7/Chapter_7.py
@@ -0,0 +1,11 @@
+# # Comments
+# Author: dhrubajyoti biswas
+
+# Escape Sequence Characters
+print("New line\nNew Line")
+print("New line\bNew Line")   # \b is used to do backspace
+print("New line\"New Line") # Double Quotes using Escape Sequence character
+print("New line\'New Line") # Single Quote using Escape Sequence character
+print("New line\rNew Line") #  Carriage Return - Moves the cursor to beginning
+print("C:\\Users\\Dhrubajyoti Biswas\\Videos\\Captures")   # backslash escape sequence character
+print("Hello \t World")  # Tab is used for indentation
